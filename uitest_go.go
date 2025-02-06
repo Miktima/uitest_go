61e1980fef4085aaa7fd57db69bccdd859c3a421
Тимофеев Михаил Константинович <m.timofeev@ria.ru>
2025-02-06 17:32:06 +0300

    test 2 init

--- uitest_go.go
+++ uitest_go.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"regexp"
 	"strings"
@@ -50,8 +51,9 @@ func main() {
 	// run task list
 	chromedp.UserAgent(config.Uagent)
 
-	// Проверка списка документов по общественным обсуждениям и оценкам регулирующего воздействия
+	// Тест № 1. Проверка списка документов по общественным обсуждениям и оценкам регулирующего воздействия
 	// на портале евразийского экономического союза
+	fmt.Println("=====> Test 1")
 
 	var baseNodes []*cdp.Node
 
@@ -65,7 +67,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Number of basenodes:", len(baseNodes))
 
 	red := regexp.MustCompile(`\d{2}.\d{2}.\d{4}`)
 
@@ -90,6 +91,7 @@ func main() {
 		if len(dept) < 10 {
 			fmt.Println("Департамент не указан в позиции - ", i)
 		}
+		// Этап должен буть указан в соответствии с датой
 		flist := red.FindAllString(dates, -1)
 		sstartdate := strings.Split(flist[0], ".")
 		senddate := strings.Split(flist[1], ".")
@@ -107,4 +109,69 @@ func main() {
 			fmt.Println("Неверный этап:", etap, " в позиции - ", i)
 		}
 	}
+
+	// Тесть № 2. Выбор департамента и проверка, что указан правильный департамент
+	fmt.Println("=====> Test 2")
+
+	err = chromedp.Run(ctx,
+		chromedp.Navigate("https://regulation.eaeunion.org/pd/"),
+		chromedp.WaitVisible(`.DocsFilter_Block:nth-child(6)`, chromedp.ByQuery),
+		// Кликаем по кнопке фильтра по департаментам
+		chromedp.Click(`//*[@id="comp_437455dd82167ef6350b8176462a33db"]/div/div[1]/div/div[2]/form/div[3]/div[1]/button`,
+			chromedp.BySearch),
+		chromedp.WaitVisible("#js-modal-filter-dept > div > div > div > form > div.Modal__Buttons > button:nth-child(1)",
+			chromedp.ByQuery),
+		// забираем ноды с департаментами
+		chromedp.Nodes("form.js-modal-filter-dept-form > div.Modal_Filter_Items > div.Checkbox", &baseNodes,
+			chromedp.ByQueryAll),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Случайным образом выбираем департамент, получает текст и id элемента
+	var av_id, dept_sv string
+	id_dept := rand.Intn(len(baseNodes))
+	ok := true
+	err = chromedp.Run(ctx,
+		chromedp.Text(".Checkbox__Title", &dept_sv, chromedp.ByQuery, chromedp.FromNode(baseNodes[id_dept])),
+		chromedp.AttributeValue(".Checkbox__Label > .Checkbox__Row > .Checkbox__Input", "id", &av_id, &ok,
+			chromedp.ByQuery, chromedp.FromNode(baseNodes[id_dept])),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Выбран департамент: %s (%s)\n", dept_sv, av_id)
+	var sshot []byte
+	var quality int
+	err = chromedp.Run(ctx,
+		// Кликаем по чекбоксу департамента
+		chromedp.Click("#"+av_id, chromedp.ByQuery),
+		chromedp.Click(`//*[@id="js-modal-filter-dept"]/div/div/div/form/div[2]/button[1]`, chromedp.BySearch),
+		chromedp.WaitVisible(".DocSearchResult_Item:nth-child(20)", chromedp.ByQuery),
+		chromedp.FullScreenshot(&sshot, quality),
+		chromedp.Nodes(".DocSearchResult_Item", &baseNodes, chromedp.ByQueryAll),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := os.WriteFile("fullScreenshot.png", sshot, 0o644); err != nil {
+		log.Fatal(err)
+	}
+
+	i = 0
+	for _, node := range baseNodes {
+		i++
+		err = chromedp.Run(ctx,
+			chromedp.Text(".DocSearchResult_Item__DatesLeft > div:nth-child(2)", &dept, chromedp.ByQuery, chromedp.FromNode(node)),
+		)
+		if err != nil {
+			log.Fatal("Error:", err)
+		}
+		// Наименование департамента должно соответствовать выбранному
+		if strings.Trim(dept, "\n\r") != strings.Trim(dept_sv, "\n\r") {
+			fmt.Printf("Департамент %s не соответствует выбранному в позиции - %d\n", dept, i)
+		}
+	}
+
+	fmt.Println("Tests completed")
 }
